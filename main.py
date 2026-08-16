c090865c2cbe330fe09f775efe4beccbcb12e387
smmaff <smmaff@yandex.kz>
2026-08-16 09:58:17 +0500

    Добавлен get endpoint для подсчета баланса и выведения его пользователю

--- main.py
+++ main.py
@@ -20,5 +20,15 @@ def add_trancaction(transaction: Transaction):
     return{"message": "Транзакция успешно добавлена!", "data": transaction}
 
 @app.get("/transactions")
-def tansaction_see():
+def transaction_see():
     return{"message": "Cписок всех транзакций: ", "data": transactions}
+
+@app.get("/balance")
+def balance_see():
+    balance = 0
+    for transaction in transactions:
+        if transaction.type == "Доход":
+            balance += transaction.amount
+        else:
+            balance -= transaction.amount
+    return{"message": "Ваш баланс:", "data": balance}
